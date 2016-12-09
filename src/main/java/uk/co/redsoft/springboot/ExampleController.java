14e6cfc4fe7815ff75756aee38bfd4b679e210c1
Nick Redshaw <nickredshaw@gmail.com>
2016-12-09 11:42:42 +0000

    Remove Spring @Value annotation way of accessing the env vars (for clarity)

--- src/main/java/uk/co/redsoft/springboot/ExampleController.java
+++ src/main/java/uk/co/redsoft/springboot/ExampleController.java
@@ -3,26 +3,26 @@ package uk.co.redsoft.springboot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @EnableAutoConfiguration
 public class ExampleController {
-
-    @Value("#{systemEnvironment['MESSAGE']}")
-    private String message;
 
     @RequestMapping("/")
     public ExampleResponse home() {
         ExampleResponse messages = new ExampleResponse();
         messages.addMessage("Java system property (server.port): [" + System.getProperty("server.port") + "]");
         messages.addMessage("Environment variable via System.getenv(MESSAGE): [" + System.getenv("MESSAGE") + "]");
-        messages.addMessage("Environment variable via @Value annotation (MESSAGE): [" + this.message + "]");
-
         return messages;
     }
 
