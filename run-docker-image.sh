ed2739d095457e5dda33c5157db082deef2d380f
Nick Redshaw <nickredshaw@gmail.com>
2016-12-09 17:23:29 +0000

    Use env var for port in Dockerfile

--- run-docker-image.sh
+++ run-docker-image.sh
@@ -5,6 +5,6 @@
 echo "$(tput setaf 3)Running Spring Boot Sandbox in Docker...$(tput sgr 0)"
 docker build -t spring-boot-sandbox .
 docker rm -f spring-boot-sandbox
-docker run -d -p 8080:8080 --name spring-boot-sandbox  spring-boot-sandbox java -Dserver.port=8080 -jar /root/app.jar
+docker run -d -p 8080:8080 --name spring-boot-sandbox  spring-boot-sandbox java -Dserver.port=$PORT -jar /root/app.jar
 docker ps
 echo "$(tput setaf 2)Click here --> http://localhost:8080/$(tput sgr 0)"
