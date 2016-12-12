d5a45fbd32e52150eabf53d7676260c5347910c1
Nick Redshaw <nickredshaw@gmail.com>
2016-12-12 11:11:32 +0000

    Rename the Docker file

--- run-docker-image.sh
+++ run-docker-image.sh
@@ -3,7 +3,7 @@
 # You must run 'mvn clean package' first to create the JAR
 
 echo "$(tput setaf 3)Running Spring Boot Sandbox in Docker...$(tput sgr 0)"
-docker build -t spring-boot-sandbox .
+docker build -f Dockerfile-vanilla -t spring-boot-sandbox .
 docker rm -f spring-boot-sandbox
 docker run -d -p 8080:8080 --name spring-boot-sandbox  spring-boot-sandbox java -Dserver.port=$PORT -jar /root/app.jar
 docker ps
